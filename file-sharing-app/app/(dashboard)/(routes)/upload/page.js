4a215dc6e7464cbd15bdb561d1bcf1756c6357a2
Praneeth <89689768+praneeth622@users.noreply.github.com>
2024-05-22 07:53:44 +0000

    files stored in firebase storage

--- file-sharing-app/app/(dashboard)/(routes)/upload/page.js
+++ file-sharing-app/app/(dashboard)/(routes)/upload/page.js
@@ -1,15 +1,39 @@
 "use client"
 import React, { useState } from 'react'
 import UploadForm from './_components/UploadForm'
-
+import {app} from '../../../../firebaseConfig'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 
 function Upload() {
   const storage =getStorage(app)
   const [uploadFile,setUploadFile] = useState()
-  const funuploadFile=(File)=>{
-    
-    setUploadFile(File)
+  const funuploadFile = (File) => {
+    console.log(File?.type);
+  
+    const metadata = {
+      contentType: File?.type
+    };
+  
+    const storageRef = ref(storage, 'File-upload/' + File?.name);
+    const uploadTask = uploadBytesResumable(storageRef, File, metadata);
+  
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log('File available at', downloadURL);
+        });
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        console.error('Upload failed:', error);
+      }
+    );
   }
+  
   return (
     <div className='p-5 px-8 md:px-28'>
       <h2 className='text-[25px] m-5 text-center'>Safe & <strong className='text-orange-500'>Easy</strong> File <strong className='text-orange-500'>Sharing</strong>.</h2>
